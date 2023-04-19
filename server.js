6787e5b0e292643431b1f39f83638ce4959ac67c
sendusyourbones <beckee.roos@gmail.com>
2023-04-19 15:39:44 -0700

    Add get routes for /notes and /api/notes

--- server.js
+++ server.js
@@ -6,8 +6,25 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+// middlewares
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Send notes.html file at path /notes
+app.get('/notes', (req, res) => {
+    res.sendFile(`${__dirname}/public/notes.html`);
+});
+
+// Read the db.json file and return all saved notes as JSON at path /api/notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile(`${__dirname}/db/db.json`, 'utf8', (err, notes) => {
+        if (err) {
+            return res.status(500).json({err});
+        }
+
+        res.json(JSON.parse(notes));
+    });
+});
 
 app.listen(PORT, () => console.log(`Server listening on PORT http://localhost:${PORT}`));

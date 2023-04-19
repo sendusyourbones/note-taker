a50524dd73de8aa480e0043f46f56c6c1a6a3576
sendusyourbones <beckee.roos@gmail.com>
2023-04-19 16:07:16 -0700

    Add POST route for /api/notes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
+const uuid = require('uuid');
 
 const app = express();
 
@@ -20,11 +21,41 @@ app.get('/notes', (req, res) => {
 app.get('/api/notes', (req, res) => {
     fs.readFile(`${__dirname}/db/db.json`, 'utf8', (err, notes) => {
         if (err) {
-            return res.status(500).json({err});
+            return res.status(500).json({ err });
         }
 
         res.json(JSON.parse(notes));
     });
+});
+
+// POST request for /api/notes
+// Receive note, assign id, add it to db.json, respond with new note
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const id = uuid.v4();
+
+        fs.readFile(`${__dirname}/db/db.json`, 'utf8', (err, notes) => {
+            if (err) {
+                return res.status(500).json({ err });
+            }
+
+            const data = JSON.parse(notes);
+
+            data.push({ title, text, id });
+
+            fs.writeFile(`${__dirname}/db/db.json`, JSON.stringify(data, null, 2), (err) => {
+                if (err) {
+                    return res.status(500).json({ err });
+                }
+
+                res.json({ title, text, id });
+            });
+        });
+    } else {
+        res.status(400).json({error: 'Title and text are required'});
+    }
 });
 
 app.listen(PORT, () => console.log(`Server listening on PORT http://localhost:${PORT}`));
